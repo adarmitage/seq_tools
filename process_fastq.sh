5656a85a08e440e962d5242d6eda189a276029fa
Richard Harrison <richard.harrison@emr.ac.uk>
2014-03-15 21:39:40 +0000

    Bug fixes- still concerned about where output files are written

--- process_fastq.sh
+++ process_fastq.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 SCRIPT_DIR=$(readlink -f ${0%/*})
+LANE=$1
+LIB=$2
+INDEX=$3
+LN=$4
+TRIM=$5
+HD=`pwd`
+echo Processing FastQ files
+mkdir $LANE"_"$TRIM
+TRIM_DIR=$LANE"_"$TRIM
+cd $LANE
 
-for FWD in $( ls -d ./lane1/LIB2429_NoIndex_L008_R1* ); do
-        #echo $FWD
+for FWD in $( ls $LIB"_"$INDEX"_"$LN"_R1"* ); do
+	cd $HD
+#	pwd
+#       echo $FWD
 	REV=$(sed -e"s/R1/R2/g" <<< $FWD) 
-	#echo $REV
-	echo "Submitting Lane 1 jobs to cluster"
-	qsub $SCRIPT_DIR/submit_process_fastq.sh $SCRIPT_DIR $FWD $REV
-	#$SCRIPT_DIR/submit_process_fastq.sh $FWD $REV
+#	echo $REV
+	echo "Submitting $LANE jobs to cluster "
+#	#qsub $SCRIPT_DIR/submit_process_fastq.sh $SCRIPT_DIR $FWD $REV
+	qsub $SCRIPT_DIR/submit_process_fastq.sh $SCRIPT_DIR $FWD $REV $LANE $TRIM_DIR
+#	$SCRIPT_DIR/submit_process_fastq.sh $SCRIPT_DIR $FWD $REV $LANE $TRIM_DIR
 done 
 
