ef1f308dd0665c6a576661570a7550534e4b2707
adarmitage <andrew.armitage.emr.ac.uk>
2014-04-11 19:04:56 +0100

    Removed changes which parsed the infile to remove DOS-made carriage returns(^M)
    Now the infile should be parsed prior to running the program using the bash command
    
    cat infilename.csv | tr ^M '\n' > infilename2.csv

--- directory_structure/build_dir.pl
+++ directory_structure/build_dir.pl
@@ -1,15 +1,17 @@
 #!/usr/bin/perl -w
 # A script to build the directories required for genomic analysis
 
+# Ensure that the following bash command has been run if the csv file was made in DOS
+# cat infilename.csv | tr ^M '\n' > infilename2.csv
+
 use strict;
 use Cwd;
 
 my ($usage) = "Usage: build_dir.pl <projectname.csv>\n\nPlease enter a comma delimited file with data in the following format:\n\nnucleic_acid,library_type,species,strain\n";
 
 my($infile) = @ARGV;
-my(@tier1_dir)= ('raw_dna', 'qc_dna', 'raw_rna', 'qc_rna', 'assembly', 'gene_pred', 'analysis', 'annotation', 'log');
+my(@tier1_dir)= ('raw_dna', 'qc_dna', 'raw_rna', 'qc_rna', 'assembly', 'gene_pred', 'analysis', 'annotation');
 my($tier1_dir);
-my($readline);
 my($currentline);
 my(@currentline);
 my($linecount)=-1;
@@ -53,7 +55,7 @@ foreach (@tier1_dir) {
 	$tier1_dir = $_;
 	mkdir "$project_name/$tier1_dir";
 }
-	
+
 foreach (@tier1_dir[4 .. 7]) {
 	$tier1_dir = $_;
 	mkdir "$project_name/$tier1_dir/prog1";
@@ -61,49 +63,30 @@ foreach (@tier1_dir[4 .. 7]) {
 
 
 
-#######  Step 2a ########
-#	Parse .csv file		#
-# 	incase it has been	#
-# 	made using Dos		#
-#########################
-
-# remove ^M from file which in Dos is used to
-# delimit \n. This is represented in unix by a \r
-# This is done by reading in the file and if a \r
-# then splitting the string 
-
-while ( $readline= <INFILE>) {
-	@currentline = split(/^M/, $readline);
-
-
-
-
-#######  Step 2b ########
+#######  Step 2 #########
 #	Read data in from	#
 # 	excel file 		#
 #########################
 
 
-	foreach (@currentline) {
-		$currentline = $_;
-		$currentline =~ s/\r\n/\n/;
-		print "\nThe current line is:\n$currentline\n";
-		if ($linecount==-1){
-			$linecount++;
-		}
-		else {
-			$currentline =~ s/\r/\n/;
-			@currentline = split (',', $currentline);
-			build_dir (@currentline, $project_name, @tier1_dir);
-			$linecount++;
-		}
+
+while ($currentline = <INFILE>) {
+	print "\nThe current line is:\n$currentline\n";
+	if ($linecount==-1){
+		$linecount++;
+	}
+	else {
+		$currentline =~ s/\n//;
+		@currentline = split (',', $currentline);
+		build_dir (@currentline, $project_name, @tier1_dir);
+		$linecount++;
 	}
 }
 
 print "The dataframe \"$project_name\" was constructed for $linecount samples provided in $infile.\n";
 
 
-	
+
 sub build_dir {
 	my (@currentline);
 		@currentline = @_[0 .. 3];
@@ -126,7 +109,7 @@ sub build_dir {
 			}
 		}		
 	}
-	
+
 	elsif ($nucleic_acid eq "rna") {
 		foreach (@tier1_dir[2, 3]) {
 			$tier1_dir = $_;
@@ -139,7 +122,7 @@ sub build_dir {
 			}
 		}
 	}
-	
+
 	foreach (@tier1_dir[4 .. 7]) {
 		$tier1_dir = $_;
 		mkdir "$project_name/$tier1_dir/prog1";
@@ -151,5 +134,3 @@ sub build_dir {
 			}
 		}	
 }
-
-
