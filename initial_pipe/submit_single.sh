cc0a057781b17b99610427ec94fd070dbc225f47
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-05-09 11:08:53 +0100

    Added submit single, a script to submit a single genome's forard and reverse reads to assembly_pipe.sh

---
+++ initial_pipe/submit_single.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Submit a single job to assembly_pipe.sh specifying a single
+# forward and reverse read from the directory structure.
+
+# Will collect Genome size and insert length value from the command line
+# if no values are given then default values are used.
+
+F_READ=$1
+R_READ=$2
+GENOME_SZ=$(if [$3] ; then; echo "$3"; else ; echo "35" ; fi)
+INS_LGTH=$(if [$4] ; then; echo "$4"; else ; echo "700" ; fi)
+	
+
+echo "submitting job for:"
+echo "$F_READ"
+echo "$R_READ"
+	
+cp $F_READ $F_READ.2.gz
+cp $R_READ $R_READ.2.gz
+
+gunzip $F_READ.2.gz
+gunzip $R_READ.2.gz 
+	
+F_INFILE=$(echo $F_READ.2 | sed 's/.gz.2//')
+R_INFILE=$(echo $R_READ.2 | sed 's/.gz.2//')
+	
+mv $F_READ.2 $F_INFILE
+mv $R_READ.2 $R_INFILE
+	
+qsub /home/armita/git_repos/seq_tools/initial_pipe/assembly_pipe.sh $F_INFILE $R_INFILE $GENOME_SZ $INS_LGTH
+
+
